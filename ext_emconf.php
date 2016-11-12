8a8d3709cec46328fb0742d8c9f5a318643ef952
René Fritz <r.fritz@colorcube.de>
2016-11-12 15:32:54 +0100

    add composer json V 0.1.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,33 +1,30 @@
 <?php
 
 $EM_CONF[$_EXTKEY] = array(
-	'title' => 'Web>Info, Content Overview',
-	'description' => 'Find Content/Plugins in page tree.',
-	'category' => 'module',
-	'author' => 'Rene Fritz',
-	'author_email' => 'r.fritz@colorcube.de',
-	'shy' => '',
-	'dependencies' => '',
-	'conflicts' => '',
-	'priority' => '',
-	'module' => '',
-	'state' => 'stable',
-	'internal' => '',
-	'uploadfolder' => 0,
-	'createDirs' => '',
-	'modify_tables' => '',
-	'clearCacheOnLoad' => 0,
-	'lockType' => '',
-	'author_company' => '',
-	'version' => '0.1.0',
-	'constraints' => array(
-		'depends' => array(
-			'typo3' => '7.4.0',
-		),
-		'conflicts' => array(
-		),
-		'suggests' => array(
-		),
-	),
+    'title' => 'Web>Info, Content Overview',
+    'description' => 'Find Content/Plugins in page tree.',
+    'category' => 'module',
+    'author' => 'René Fritz',
+    'author_email' => 'r.fritz@colorcube.de',
+    'author_company' => 'Colorcube',
+    'state' => 'stable',
+    'internal' => '',
+    'uploadfolder' => 0,
+    'createDirs' => '',
+    'modify_tables' => '',
+    'clearCacheOnLoad' => 0,
+    'lockType' => '',
+    'version' => '0.1.0',
+    'constraints' => array(
+        'depends' => array(
+            'typo3' => '7.4.0-7.99.99',
+        ),
+        'conflicts' => array(),
+        'suggests' => array(),
+    ),
+    'autoload' => array(
+        'psr-4' => array(
+            'Colorcube\\ContentOverview\\' => 'Classes'
+        ),
+    ),
 );
-?>
