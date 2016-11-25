84991ac8f514626c2b6a5611689d6c81d36a6f13
René Fritz <r.fritz@colorcube.de>
2016-11-25 15:33:04 +0100

    fix composer.json

--- ext_emconf.php
+++ ext_emconf.php
@@ -2,7 +2,7 @@
 
 $EM_CONF[$_EXTKEY] = array(
     'title' => 'Web>Info, Content Overview',
-    'description' => 'Find Content/Plugins in page tree.',
+    'description' => 'Backend module which gives and overview of the the content elements in a page tree.',
     'category' => 'module',
     'author' => 'René Fritz',
     'author_email' => 'r.fritz@colorcube.de',
@@ -14,7 +14,7 @@ $EM_CONF[$_EXTKEY] = array(
     'modify_tables' => '',
     'clearCacheOnLoad' => 0,
     'lockType' => '',
-    'version' => '0.1.0',
+    'version' => '0.1.1',
     'constraints' => array(
         'depends' => array(
             'typo3' => '7.4.0-7.99.99',
